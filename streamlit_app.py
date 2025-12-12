a3d3e91b3f129fc41d64968757b4d8d0e1ac9271
Nour Garali <nourgarali12345@gmail.com>
2025-12-12 22:39:20 +0100

    Initial commit for HuggingFace Docker deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ from plotly.subplots import make_subplots
 # -----------------------------
 # CONFIGURATION (modifiable)
 # -----------------------------
-MODEL_PATH = "best_gaze_model.keras"
+MODEL_PATH = "best_gaze_model.h5"
 DURATION_SECONDS = 30
 FPS_TARGET = 15
 WINDOW_SEC = 3
